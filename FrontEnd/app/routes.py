a564563b58ec1cc04ef7e64d9a2ff669a2f41d03
btr26 <btr26@txstate.edu>
2018-12-04 20:29:16 -0600

    Can now record an audio file, save it, and upload it to the backend server. Saved under FrontEnd/app/audio_upload

--- FrontEnd/app/routes.py
+++ FrontEnd/app/routes.py
@@ -2,8 +2,9 @@ from flask import render_template, send_file
 from flask import Flask, flash, request, redirect, url_for
 from app import app
 import os
+from os.path import join
 
-basedir = os.path.abspath(os.path.dirname(__file__))
+basedir = os.path.dirname(os.path.abspath(__file__))
 
 @app.route('/')
 @app.route('/index')
@@ -11,28 +12,17 @@ def index():
     return render_template('index.html')
 
 @app.route('/upload', methods=['GET', 'POST'])
-def upload_file():
-    if request.method == 'POST':
-        # check if the post request has the file part
-        if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
-        file = request.files['file']
-        # if user does not select file, browser also
-        # submit an empty part without filename
-        if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
-        if file and allowed_file(file.filename):
-			filename = secure_filename(file.filename)
-			file.save(os.path.join('/', filename))
-			return redirect(url_for('uploaded_file', filename=filename))
-    return '''
-    <!doctype html>
-    <title>Upload new File</title>
-    <h1>Upload new File</h1>
-    <form method=post enctype=multipart/form-data>
-      <input type=file name=file>
-      <input type=submit value=Upload>
-    </form>
-    '''
+def upload():
+    target = os.path.join(basedir, 'audio_upload/')
+    print(target)
+
+    if not os.path.isdir(target):
+        os.mkdir(target)
+    
+    for file in request.files.getlist("file"):
+        filename = file.filename
+        print(filename)
+        destination = "/" + target + filename
+        print(destination)
+        file.save(destination)
+    return render_template("uploaded_file.html")
